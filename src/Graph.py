df597c0ad49b4f965e4900272957d83773c4d47c
Yuval Nissan <yuval.nissan4@gmail.com>
2018-03-01 22:38:26 +0200

    find_shortest path has no input now

--- src/Graph.py
+++ src/Graph.py
@@ -7,13 +7,15 @@ class Graph:
 
     def __init__(self, R, C, T):
 
+        self.end_node = (0,0,T)
+
         dg = nx.DiGraph()
         for t in range(T):
             for c in range(C):
                 for r in range(R):
                     dg.add_node((r, c, t))
 
-        dg.add_node((0,0,T))
+        dg.add_node(self.end_node)
         for node in dg.nodes:
             r, c, t = node
             if t < T - 1:
@@ -29,7 +31,7 @@ class Graph:
                     dg.add_edge(node, (r - 1, c, t + 1), weight=1)
 
             else:
-                dg.add_edge(node, (0,0,T), weight=0)
+                dg.add_edge(node, self.end_node, weight=0)
 
         # nx.draw(dg)
         # plt.show()
@@ -39,9 +41,9 @@ class Graph:
         # TODO
         pass
 
-    def find_shortest_path(self, end):
+    def find_shortest_path(self):
         try:
-            path_of_nodes = nx.dijkstra_path(self.dg, (0,0,0), end)
+            path_of_nodes = nx.dijkstra_path(self.dg, (0,0,0), self.end_node)
             path_of_edges = []
             for i in range(len(path_of_nodes) - 1):
                 edge = (path_of_nodes[i], path_of_nodes[i+1])
