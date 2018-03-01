7952eb997951f983c128fb6ce7fa5a6ff47da6e9
amitaydr <amitaydr@gmail.com>
2018-03-01 20:48:27 +0200

    com1

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+import networkx
+
+def point_dist(p1, p2):
+    return abs(p1[0]-p2[0])+abs(p1[1]-p2[1])
+
+
+def ride_distance(ride):
+    return point_dist(ride['startPoint'], ride['endPoint'])
+
+def build_graph(rides, c, r, t):
